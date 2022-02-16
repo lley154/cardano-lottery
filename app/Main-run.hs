9606fe4522bd9a31c2ae0cbe792ffa9f60d60936
lley154 <lawrence.ley@contextsolutions.ca>
2022-02-16 16:29:05 -0500

    - updated Main-run.hs to use devnet testing configs

---
+++ app/Main-run.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+
+module Main
+    ( main
+    ) where
+
+import          LottoContract                                    (StarterContracts(..))
+import          Plutus.PAB.Effects.Contract.Builtin qualified as Builtin
+import          Plutus.PAB.LocalCluster.Run         (runWith)
+--import Plutus.PAB.Run (runWith)
+
+
+main :: IO ()
+main = do
+    runWith (Builtin.handleBuiltin @StarterContracts)
+
+
